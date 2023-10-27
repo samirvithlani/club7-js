92b76644b8be21b8717eec440de651c404d1332d
samirvithlani <samir.vithlani83955@gmail.com>
2023-10-27 16:51:19 +0530

    event listner

--- eventlistners/eventlistner.js
+++ eventlistners/eventlistner.js
@@ -1,28 +1,25 @@
-document.getElementById("btn").addEventListener("click",()=>{
+document.getElementById("btn").addEventListener("click", () => {
+  console.log("clicked");
+});
 
-console.log("clicked")
+document.getElementById("box1").addEventListener("mouseover", () => {
+  document.getElementById("box1").className = "box";
+});
 
-})
+document.getElementById("fb").addEventListener("dblclick", () => {
+  document.getElementById("fb").href = "https://www.facebook.com/";
+  document.getElementById("fbimg").src = "./fb.jpg";
+});
+document.getElementById("google").addEventListener("dblclick", () => {
+  document.getElementById("google").href = "https://www.google.com/";
+  document.getElementById("gimg").src = "./google.png";
+});
 
-document.getElementById("box1").addEventListener("mouseover",()=>{
 
-    document.getElementById("box1").className="box"
-})
 
-document.getElementById("fb").addEventListener("dblclick",()=>{
-
-    document.getElementById("fb").href="https://www.facebook.com/"
-    document.getElementById("fbimg").src ="./fb.jpg"
-
-})
-document.getElementById("google").addEventListener("dblclick",()=>{
-
-    document.getElementById("google").href="https://www.google.com/"
-    document.getElementById("gimg").src ="./google.png"
-})
-
+/// 10 8 th event ---> event listner
 // document.getElementById("btn").addEventListener("click",function(){
-    
+
 // })
 // const test =()=>
 // {
