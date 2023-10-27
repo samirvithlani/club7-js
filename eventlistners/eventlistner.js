255997618f45f302024da1663ec3a8497d673352
samirvithlani <samir.vithlani83955@gmail.com>
2023-10-27 16:41:29 +0530

    eventlistners

---
+++ eventlistners/eventlistner.js
@@ -0,0 +1,31 @@
+document.getElementById("btn").addEventListener("click",()=>{
+
+console.log("clicked")
+
+})
+
+document.getElementById("box1").addEventListener("mouseover",()=>{
+
+    document.getElementById("box1").className="box"
+})
+
+document.getElementById("fb").addEventListener("dblclick",()=>{
+
+    document.getElementById("fb").href="https://www.facebook.com/"
+    document.getElementById("fbimg").src ="./fb.jpg"
+
+})
+document.getElementById("google").addEventListener("dblclick",()=>{
+
+    document.getElementById("google").href="https://www.google.com/"
+    document.getElementById("gimg").src ="./google.png"
+})
+
+// document.getElementById("btn").addEventListener("click",function(){
+    
+// })
+// const test =()=>
+// {
+
+// }
+// document.getElementById("btn").addEventListener("click",test)
